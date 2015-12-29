3b5ea6da772a45a743a6cee574573ca068a2d1cf
Jesse Sipola <jsipola@gmail.com>
2015-12-29 15:10:22 +0200

    Initial Commit

---
+++ Reittiopas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reittiopas
+//
+//  Created by Jesse Sipola on 29.12.2015.
+//  Copyright © 2015 Jesse Sipola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
